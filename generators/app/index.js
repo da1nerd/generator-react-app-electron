2f688e68d3f0e37e0f6135d03ca0c2d039532598
Joel Lonbeck <joel@neutrinographics.com>
2019-03-08 18:03:16 -0800

    added templates

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,59 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const askName = require("inquirer-npm-name");
+const path = require("path");
+const mkdirp = require("mkdirp");
+const fs = require("fs-extra");
+const _ = require("lodash");
+
+function makeProjectName(name) {
+  return _.kebabCase(name);
+}
 
 module.exports = class extends Generator {
+  initializing() {
+    this.props = {};
+  }
+
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the unreal ${chalk.red('generator-react-app-electron')} generator!`)
-    );
-
-    const prompts = [
+    return askName(
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+        name: "name",
+        message: "Your project name",
+        default: makeProjectName(path.basename(process.cwd())),
+        filter: makeProjectName,
+        validate: str => {
+          return str.length > 0;
+        }
+      },
+      this
+    ).then(props => {
+      this.props.name = props.name;
     });
   }
 
+  default() {
+    if (path.basename(this.destinationPath()) !== this.props.name) {
+      this.log(
+        `Your project folder does not exist.\nI'll automatically create this folder.`
+      );
+      mkdirp(this.props.name);
+      this.destinationRoot(this.destinationPath(this.props.name));
+    }
+  }
+
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    fs.copySync(
+      this.templatePath(""),
+      this.destinationPath("")
     );
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      bower: false,
+      npm: false,
+      yarn: true
+    });
   }
 };
