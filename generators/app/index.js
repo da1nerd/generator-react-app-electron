8a4f1374339d5578ea5ec6086411d7dd9978f3c1
Joel <joel@neutrinographics.com>
2019-10-15 11:43:04 +0700

    updated more deps

--- generators/app/index.js
+++ generators/app/index.js
@@ -24,6 +24,7 @@ module.exports = class extends Generator {
     if (this.options.name) {
       return Promise.resolve({ name: this.options.name });
     }
+
     return askName(
       {
         name: "name",
